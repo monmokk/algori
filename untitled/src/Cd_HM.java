a6883a95fca24505b93d7493c1d4a004c674f24f
mjk <myeongjikang910814@gmail.com>
2022-04-02 23:08:30 +0900

    운동

--- untitled/src/Cd_HM.java
+++ untitled/src/Cd_HM.java
@@ -11,9 +11,7 @@ public class Cd_HM {
         }
         for (String player : completion) {
             map.put(player, map.get(player) - 1);
-            Iterator<Map.Entry<String, Integer>> itr = map.entrySet().iterator();
-            while (itr.hasNext()) {
-                Map.Entry<String, Integer> entry = itr.next();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
                 if (entry.getValue() != 0) {
                     answer = entry.getKey();
                     break;
@@ -23,10 +21,26 @@ public class Cd_HM {
         return answer;
     }
 
+    public String progressiveSolution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) map.put(player, map.getOrDefault(player, 0) + 1);
+        for (String player : completion) map.put(player, map.get(player) - 1);
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         String[] part = {"leo", "kiki", "eden"};
         String[] comp = {"eden", "kiki"};
         Cd_HM cd_hm= new Cd_HM();
         System.out.println(cd_hm.solution(part, comp));
+        System.out.println(cd_hm.progressiveSolution(part, comp));
     }
 }
