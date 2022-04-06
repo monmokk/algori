523a53ee5804c1fc6682e33238cc82040c01976f
mjk <myeongjikang910814@gmail.com>
2022-04-06 16:19:11 +0900

    스트림 공부즁

--- untitled/src/PracticeStream.java
+++ untitled/src/PracticeStream.java
@@ -3,15 +3,22 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class PracticeStream {
-    public static void main(String[] args) {
+
+    public static void print(String str) {
+        System.out.println(str+" - "+Thread.currentThread().getName());
+    }
+
+    void practiceStr1(){
         List<String> list = Arrays.asList("pp", "mm", "jj");
         Stream<String> stream = list.stream();
         stream.forEach(System.out::println); //메서드 참조
         //stream.forEach(name -> System.out.println(name));
+    }
 
+    void practiceStrFor(){
         List<Student> list1 = Arrays.asList(
-            new Student("퐁", 0),
-            new Student("찌오니", 100)
+                new Student("퐁", 0),
+                new Student("찌오니", 100)
         );
 
         Stream<Student> stream1 = list1.stream();
@@ -20,7 +27,9 @@ public class PracticeStream {
             int score = student.getScore();
             System.out.println(name + "-" + score);
         });
+    }
 
+    void practiceStrM(){
         List<String> nameList = Arrays.asList(
                 "예제", "만드는중","호옹", "글쿤"
         );
@@ -30,7 +39,11 @@ public class PracticeStream {
         parallelStream.forEach(PracticeStream::print);
     }
 
-    public static void print(String str) {
-        System.out.println(str+" - "+Thread.currentThread().getName());
+
+    public static void main(String[] args) {
+        PracticeStream practiceStream = new PracticeStream();
+        practiceStream.practiceStr1();
+        practiceStream.practiceStrFor();
+        practiceStream.practiceStrM();
     }
 }
