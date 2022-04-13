4bf0868120f7dae2964387f7c4b87bac3a0a6a36
mjk <myeongjikang910814@gmail.com>
2022-04-13 23:44:58 +0900

    N으로 표현

--- untitled/src/ExpressN.java
+++ untitled/src/ExpressN.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExpressN {
     private int n;
     private int target;
@@ -32,15 +35,53 @@ public class ExpressN {
         }
     }
 
+    public int dpSolution(int N, int number) {
+        int answer = -1;
+        Set<Integer>[] sets = new Set[9];
+        int t = N;
+        for (int i = 1; i < 9 ; i++) {
+            sets[i] = new HashSet<>();
+            sets[i].add(t);
+            t = t * 10 + N;
+        }
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < i; j++) {
+                for (Integer a : sets[j]) {
+                    for (Integer b : sets[i - j]) {
+                        sets[i].add(a + b);
+                        sets[i].add(a - b);
+                        sets[i].add(b - a);
+                        sets[i].add(a * b);
+                        if (b != 0) {
+                            sets[i].add(a / b);
+                        }
+                        if (a != 0) {
+                            sets[i].add(b / a);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <9 ; i++) {
+            if (sets[i].contains(number)) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
         ExpressN expressN = new ExpressN();
         int n = 5;
         int number = 12;
 
         System.out.println(expressN.solution(n, number));
+        System.out.println(expressN.dpSolution(n,number));
         n = 2;
         number = 11;
         System.out.println(expressN.solution(n, number));
+        System.out.println(expressN.dpSolution(n,number));
     }
 }
 
