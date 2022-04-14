d0e71ff8b1438c4628a8a79599dedb7b88771010
mjk <myeongjikang910814@gmail.com>
2022-04-14 22:53:20 +0900

    queue

--- untitled/src/FunctionImprovement.java
+++ untitled/src/FunctionImprovement.java
@@ -30,6 +30,27 @@ public class FunctionImprovement {
         return answer;
     }
 
+    public int[] queueSolution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int day = queue.poll();
+            int cnt = 1;
+
+            while (!queue.isEmpty() && day >= queue.peek()) {
+                cnt++;
+                queue.poll();
+            }
+            answer.add(cnt);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
     public static void main(String[] args) {
 
         FunctionImprovement functionImprovement = new FunctionImprovement();
@@ -38,13 +59,17 @@ public class FunctionImprovement {
         int[] speeds = {1, 30, 5};
 
         String str = Arrays.toString(functionImprovement.solution(progresses, speeds));
+        String qStr = Arrays.toString(functionImprovement.queueSolution(progresses, speeds));
         System.out.println(str);
+        System.out.println(qStr);
 
         progresses = new int[]{95, 90, 99, 99, 80, 99};
         speeds = new int[]{1, 1, 1, 1, 1, 1};
 
         str = Arrays.toString(functionImprovement.solution(progresses, speeds));
+        qStr = Arrays.toString(functionImprovement.queueSolution(progresses, speeds));
         System.out.println(str);
+        System.out.println(qStr);
 
     }
 }
