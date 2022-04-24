8e827c8ee1d5ae48c72e429c8bfaf93f0531a98c
mjk <myeongjikang910814@gmail.com>
2022-04-25 01:00:33 +0900

    queue 트럭

--- untitled/src/Truck.java
+++ untitled/src/Truck.java
@@ -1,7 +1,61 @@
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Queue;
 
 public class Truck {
+    static class SubTruck {
+        int weight;
+        int move;
+
+        public SubTruck(int weight) {
+            this.weight = weight;
+            this.move = 1;
+        }
+
+        public void moving() {
+            move++;
+        }
+    }
+    public int progressiveSolution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+
+        Queue<SubTruck> waitQ = new LinkedList<>();
+        Queue<SubTruck> moveQ = new LinkedList<>();
+
+        for (int t : truck_weights) {
+            waitQ.offer(new SubTruck(t));
+        }
+
+        int currentWeight = 0;
+
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+            answer++;
+
+            if (moveQ.isEmpty()) {
+                SubTruck truck = waitQ.poll();
+                currentWeight += truck.weight;
+                moveQ.offer(truck);
+                continue;
+            }
+
+            for (SubTruck truck : moveQ) {
+                truck.moving();
+            }
+
+            if (Objects.requireNonNull(moveQ.peek()).move > bridge_length) {
+                SubTruck truck = moveQ.poll();
+                currentWeight -= truck.weight;
+            }
+
+            if (!waitQ.isEmpty() && currentWeight + waitQ.peek().weight <= weight) {
+                SubTruck truck = waitQ.poll();
+                currentWeight += truck.weight;
+                moveQ.offer(truck);
+            }
+        }
+
+        return answer;
+    }
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         Queue<Integer> queue = new LinkedList<>();
@@ -41,5 +95,6 @@ public class Truck {
         int weight = 10;
         int[] truck_weights = {7, 4, 5, 6};
         System.out.println(truck.solution(bridge_length, weight, truck_weights));
+        System.out.println(truck.progressiveSolution(bridge_length, weight, truck_weights));
     }
 }
